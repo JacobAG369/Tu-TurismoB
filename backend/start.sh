ce9d031c91065de99e4131e25f84f4dfe87c6a41
JacobAG369 <jmy.369@outlook.com>
2026-03-26 00:36:38 -0600

    render6

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ejecutar descubrimiento de paquetes ahora que hay variables de entorno (.env de Render)
+php artisan package:discover --ansi
+
 # Generar cache para producción (opcional pero recomendado)
 php artisan config:cache
 php artisan route:cache
